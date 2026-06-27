2e70399c289b648415247fa2fd875238876058cf
Ezio <e@e416.dev>
2026-06-27 13:58:52 -0600

    more lexing

--- new/baker/lexer/lexer.py
+++ new/baker/lexer/lexer.py
@@ -10,9 +10,14 @@ __all__ = [
 ]
 
 
+IDENTIFIER_SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_'
+LOGIC_KEYWORDS     = 'and', 'nand', 'nor', 'not', 'nox', 'or', 'xor'
 NUMBER_SYMBOLS     = "0123456789'."
-OPERATOR_SYMBOLS   = '%*-+=/'
-PUNCTUATOR_SYMBOLS = r'{}[]();,'
+OPERATOR_SYMBOLS   = '=+-*/%!&|^<>'
+PUNCTUATOR_SYMBOLS = r'{}[]();,"'
+SPECIAL_KEYWORDS   = 'as', 'break', 'case', 'cast', 'continue', 'do', 'else', 'false', 'for', 'if', 'in', 'return',\
+    'switch', 'true', 'while', 'with'
+TYPE_KEYWORDS      = 'bool', 'char', 'f32', 'f64', 'i8', 'i16', 'i32', 'i64', 'u8', 'u16', 'u32', 'u64', 'void'
 
 
 class LexerError(RuntimeError):
@@ -52,20 +57,40 @@ def tokenize(source: list[reader.SourceFile]) -> list[Token]:
                     while line.next() in NUMBER_SYMBOLS:
                         line.take()
                     if line.taken()[0] not in NUMBER_SYMBOLS:
-                        raise LexerError('numbers must start with a digit')
+                        raise LexerError(f'numbers must start with a digit: "{line.taken()}"')
                     if line.taken().count('.') > 1:
-                        raise LexerError('numbers can have, at most, one decimal point')
+                        raise LexerError(f'numbers can have a maximum of one decimal point: "{line.taken()}"')
                     tokens.append(Token('Number', line))
 
                 elif line.next() in OPERATOR_SYMBOLS:
                     op = line.take()
-                    if op == '*' and line.next() == "*":
-                        line.take()
+                    for char in '=*&|^<>':
+                        if op == char and line.next() in char + '=':
+                            line.take()
+                            break
+                    for char in '+-':
+                        if op == char and line.next() in char:
+                            line.take()
+                            break
                     tokens.append(Token('Operator', line))
 
                 elif line.next() in PUNCTUATOR_SYMBOLS:
                     line.take()
                     tokens.append(Token('Punctuator', line))
+
+                elif line.next() in IDENTIFIER_SYMBOLS:
+                    while line.next() in IDENTIFIER_SYMBOLS:
+                        line.take()
+                    if line.taken()[0] in NUMBER_SYMBOLS:
+                        raise LexerError(f'identifiers cannot start with a digit: "{line.taken()}"')
+                    if line.taken() in SPECIAL_KEYWORDS:
+                        tokens.append(Token('Special', line))
+                    elif line.taken() in TYPE_KEYWORDS:
+                        tokens.append(Token('Type', line))
+                    elif line.taken() in LOGIC_KEYWORDS:
+                        tokens.append(Token('Operator', line))
+                    else:
+                        tokens.append(Token('Identifier', line))
 
                 else:
                     raise LexerError(
--- new/test/main.cake
+++ new/test/main.cake
@@ -1,1 +1,10 @@
-5 + 6;
+void main() {
+    i32 i = 0;
+    i &= 3;
+    if (i == 3) {
+        return 0;
+    }
+    i++;
+    i >> 4;
+    bool b = true || false;
+}
